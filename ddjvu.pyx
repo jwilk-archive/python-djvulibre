fc0ef9db1c3a91885e1f5ecaf06619cd8d2b676e
Jakub Wilk <jwilk@jwilk.net>
2007-10-09 19:11:23 +0000

    ddjvu: drop support for resetting `Context.cache_size`; major additions. - Add `Context_from_c()` function. - Add `InstantiationError` exception. - Add `Document` class (sprout) and `Document_from_c()` function. - Add `Page` class (sprout) and `Page_from_c()` function. - Add `Job` class (sprout) and `Job_from_c()` function. - Add `Stream` class. - Add `Message` class and its subclassess:   - `ErrorMessage`;   - `InfoMessage`;   - `NewStreamMessage`;   - `DocInfoMessage` (sprout);   - `PageInfoMessage` (sprout);   - `RelayOutMessage` (sprout);   - `ChunkMessage` class (sprout);   - `ThumbnailMessage` (sprout);   - `ProgressMessage` (sprout).

--- ddjvu.pyx
+++ ddjvu.pyx
@@ -1,5 +1,8 @@
 # Copyright (c) 2007 Jakub Wilk <ubanus@users.sf.net>
 
+cdef extern from 'Python.h':
+	int PyString_AsStringAndSize(object, char**, int*) except -1
+
 cdef extern from 'stdio.h':
 	struct _IO_FILE
 	ctypedef _IO_FILE FILE
@@ -286,20 +289,25 @@ cdef extern from 'libdjvu/ddjvuapi.h':
 		ddjvu_message_progress_s m_progress
 
 
+cdef object the_sentinel
+the_sentinel = object()
+
+
 cdef class Context:
 
 	cdef ddjvu_context_t* ddjvu_context
 	cdef unsigned long DEFAULT_CACHE_SIZE
 
-	def __new__(self, argv0 = None):
+	def __new__(self, argv0 = None, **kwargs):
+		if kwargs.get('sentinel') is the_sentinel:
+			return
 		if argv0 is None:
 			from sys import argv
 			argv0 = argv[0]
 		self.ddjvu_context = ddjvu_context_new(argv0)
 		if self.ddjvu_context == NULL:
 			raise MemoryError
-		self.DEFAULT_CACHE_SIZE = ddjvu_cache_get_size(self.ddjvu_context)
-
+	
 	property cache_size:
 
 		def __set__(self, value):
@@ -312,13 +320,202 @@ cdef class Context:
 		def __get__(self):
 			return ddjvu_cache_get_size(self.ddjvu_context)
 
-		def __del__(self):
-			ddjvu_cache_set_size(self.ddjvu_context, self.DEFAULT_CACHE_SIZE)
-
 	def clear_cache(self):
 		ddjvu_cache_clear(self.ddjvu_context)
 
 	def __dealloc__(self):
 		ddjvu_context_free(self.ddjvu_context)
+
+cdef Context Context_from_c(ddjvu_context_t* ddjvu_context):
+	cdef Context result
+	result = Context(the_sentinel)
+	result.ddjvu_context = ddjvu_context
+	return result
+
+
+class InstantiationError(RuntimeError):
+	pass
+
+
+cdef class Document:
+
+	cdef ddjvu_document_t* ddjvu_document
+	
+	def __new__(self, **kwargs):
+		if kwargs.get('sentinel') is not the_sentinel:
+			raise InstantiationError
+		self.ddjvu_document = NULL
+
+cdef Document Document_from_c(ddjvu_document_t* ddjvu_document):
+	cdef Document result
+	result = Document(the_sentinel)
+	result.ddjvu_document = ddjvu_document
+	return result
+
+
+cdef class Page:
+	
+	cdef ddjvu_page_t* ddjvu_page
+	
+	def __new__(self, **kwargs):
+		if kwargs.get('sentinel') is not the_sentinel:
+			raise InstantiationError
+		self.ddjvu_page = NULL
+
+cdef Page Page_from_c(ddjvu_page_t* ddjvu_page):
+	cdef Page result
+	result = Page(the_sentinel)
+	result.ddjvu_page = ddjvu_page
+	return result
+
+
+cdef class Job:
+
+	cdef ddjvu_job_t* ddjvu_job
+	
+	def __new__(self, **kwargs):
+		if kwargs.get('sentinel') is not the_sentinel:
+			raise InstantiationError
+		self.ddjvu_job = NULL
+
+cdef Job Job_from_c(ddjvu_job_t* ddjvu_job):
+	cdef Job result
+	result = Job(the_sentinel)
+	result.ddjvu_job = ddjvu_job
+	return result
+
+
+cdef class Message:
+
+	cdef ddjvu_message_t* ddjvu_message
+	cdef Context _context
+	cdef Document _document
+	cdef Page _page
+	cdef Job _job
+
+	def __new__(self, **kwargs):
+		if kwargs.get('sentinel') is not the_sentinel:
+			raise InstantiationError
+		self.ddjvu_message = NULL
+	
+	def __init(self):
+		if self.ddjvu_message == NULL:
+			raise SystemError
+		self._context = Context_from_c(self.ddjvu_message.m_any.context)
+		self._document = Document_from_c(self.ddjvu_message.m_any.document)
+		self._page = Page_from_c(self.ddjvu_message.m_any.page)
+		self._job = Job_from_c(self.ddjvu_message.m_any.job)
+	
+	property context:
+		def __get__(self):
+			return self._context
+
+	property document:
+		def __get__(self):
+			return self._document
+
+	property page:
+		def __get__(self):
+			return self._page
+
+	property job:
+		def __get__(self):
+			return self._job
+
+cdef class ErrorMessage(Message):
+
+	property message:
+		def __get__(self):
+			return self.ddjvu_message.m_error.message
+	
+	property location:
+		def __get__(self):
+			return \
+			(
+				self.ddjvu_message.m_error.function,
+				self.ddjvu_message.m_error.filename,
+				self.ddjvu_message.m_error.lineno
+			)
+
+cdef class InfoMessage(Message):
+	
+	property message:
+		def __get__(self):
+			return self.ddjvu_message.m_info.message
+	
+cdef class Stream:
+
+	cdef int streamid
+	cdef Document document
+
+	def __new__(self, Document document not None, int streamid, **kwargs):
+		if kwargs.get('sentinel') is not the_sentinel:
+			raise InstantiationError
+		self.streamid = streamid
+		self.document = document
+
+	def close(self):
+		ddjvu_stream_close(self.document.ddjvu_document, self.streamid, 0)
+	
+	def abort(self):
+		ddjvu_stream_close(self.document.ddjvu_document, self.streamid, 1)
+	
+	def flush(self):
+		pass
+
+	def read(self, size = None):
+		raise IOError
+	
+	def write(self, data):
+		cdef char* raw_data
+		cdef int length
+		PyString_AsStringAndSize(data, &raw_data, &length)
+		ddjvu_stream_write(self.document.ddjvu_document, self.streamid, raw_data, length)
+
+	def __dealloc__(self):
+		if <object>self.document is None:
+			return
+		ddjvu_stream_close(self.document.ddjvu_document, self.streamid, 1)
+
+cdef class NewStreamMessage(Message):
+
+	cdef Stream _stream
+
+	def __init(self):
+		Message.__init(self)
+		self._stream = Stream(self.document, self.ddjvu_message.m_newstream.streamid, sentinel = the_sentinel)
+
+	property stream:
+		def __get__(self):
+			return self._stream
+	
+	property name:
+		def __get__(self):
+			return self.ddjvu_message.m_newstream.name
+	
+	property uri:
+		def __get__(self):
+			return self.ddjvu_message.m_newstream.url
+
+cdef class DocInfoMessage(Message):
+	pass
+
+cdef class PageInfoMessage(Message):
+	pass
+
+cdef class RelayOutMessage(Message):
+	pass
+
+cdef class RedisplayMessage(Message):
+	pass
+
+cdef class ChunkMessage(Message):
+	pass
+
+cdef class ThumbnailMessage(Message):
+	pass
+
+cdef class ProgressMessage(Message):
+	pass
 
 # vim:ts=4 sw=4 noet
--- tests/ddjvu.py
+++ tests/ddjvu.py
@@ -25,16 +25,52 @@ class ContextTest:
 	ValueError: 0 < cache_size < 2 * (sys.maxint + 1) is not satisfied
 	>>> x.cache_size == (1 << 32) - 1
 	True
-	>>> del x.cache_size
-	>>> x.cache_size
-	10485760L
 	>>> x.clear_cache()
 	>>> del x
+	'''
+
+class DocumentTest:
+	'''
+	>>> Document()
+	Traceback (most recent call last):
+	...
+	InstantiationError
+	'''
+
+class PageTest:
+	'''
+	>>> Page()
+	Traceback (most recent call last):
+	...
+	InstantiationError
+	'''
+
+class JobTest:
+	'''
+	>>> Job()
+	Traceback (most recent call last):
+	...
+	InstantiationError
+	'''
+
+class MessageTest:
+	'''
+	>>> Message()
+	Traceback (most recent call last):
+	...
+	InstantiationError
+	'''
+
+class StreamTest:
+	'''
+	>>> Stream(None, 42)
+	Traceback (most recent call last):
+	...
+	TypeError: Argument 'document' has incorrect type (expected ddjvu.Document, got NoneType)
 	'''
 
 if __name__ == '__main__':
 	import doctest
 	doctest.testmod()
-
 
 # vim:ts=4 sw=4 noet
