b1c2b20cf78cfeaa29498e1dcab323ea99871f3c
Jakub Wilk <jwilk@jwilk.net>
2008-03-28 20:30:08 +0000

    const: fix a typo.

--- djvu/const.py
+++ djvu/const.py
@@ -6,7 +6,7 @@
 METADATA_BIBTEX_KEYS = set(('address', 'author', 'booktitle', 'chapter', 'edition', 'editor', 'howpublished', 'institution', 'journal', 'key', 'month', 'note', 'number', 'organization', 'pages', 'publisher', 'school', 'series', 'title', 'type', 'volume', 'year'))
 # Retrieved from <http://nwalsh.com/tex/texhelp/bibtx-7.html>
 
-METADATA_PDFINFO_KEYS = set(('Author', 'CreationDate', 'Creator', 'Keywords', 'ModDate' 'Producer', 'Subject', 'Title', 'Trapped'))
+METADATA_PDFINFO_KEYS = set(('Author', 'CreationDate', 'Creator', 'Keywords', 'ModDate', 'Producer', 'Subject', 'Title', 'Trapped'))
 # Retrived from the PDF specification
 
 METADATA_KEYS = METADATA_BIBTEX_KEYS | METADATA_PDFINFO_KEYS
