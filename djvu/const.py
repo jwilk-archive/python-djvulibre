955ce36fd2b75457eb3961d4d91bb3c4ee594021
Jakub Wilk <jwilk@jwilk.net>
2008-04-17 15:08:15 +0000

    const: add `ZONE_SEPARATORS`.

--- djvu/const.py
+++ djvu/const.py
@@ -3,6 +3,8 @@
 
 '''DjVuLibre bindings: various constants.'''
 
+import djvu.decode
+
 METADATA_BIBTEX_KEYS = set(('address', 'author', 'booktitle', 'chapter', 'edition', 'editor', 'howpublished', 'institution', 'journal', 'key', 'month', 'note', 'number', 'organization', 'pages', 'publisher', 'school', 'series', 'title', 'type', 'volume', 'year'))
 # Retrieved from <http://nwalsh.com/tex/texhelp/bibtx-7.html>
 
@@ -10,5 +12,22 @@ METADATA_PDFINFO_KEYS = set(('Author', 'CreationDate', 'Creator', 'Keywords', 'M
 # Retrived from the PDF specification
 
 METADATA_KEYS = METADATA_BIBTEX_KEYS | METADATA_PDFINFO_KEYS
+
+ZONE_SEPARATORS = \
+{
+	djvu.decode.TEXT_ZONE_PAGE:      '\f',   # Form Feed (FF)
+	djvu.decode.TEXT_ZONE_COLUMN:    '\v',   # Vertical tab (VT, LINE TABULATION)
+	djvu.decode.TEXT_ZONE_REGION:    '\035', # Group Separator (GS, INFORMATION SEPARATOR THREE)
+	djvu.decode.TEXT_ZONE_PARAGRAPH: '\037', # Unit Separator (US, INFORMATION SEPARATOR ONE)
+	djvu.decode.TEXT_ZONE_LINE:      '\n',   # Line Feed (LF)
+	djvu.decode.TEXT_ZONE_WORD:      ' ',    # space
+	djvu.decode.TEXT_ZONE_CHARACTER: ''
+}
+
+__all__ = \
+(
+	'METADATA_BIBTEX_KEYS', 'METADATA_PDFINFO_KEYS', 'METADATA_KEYS',
+	'ZONE_SEPARATORS',
+)
 
 # vim:ts=4 sw=4 noet
