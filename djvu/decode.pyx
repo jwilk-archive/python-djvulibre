071eab90e1fef1da2a0744632ec40de51322513f
Jakub Wilk <jwilk@jwilk.net>
2010-10-12 23:41:51 +0200

    Set "cython: autotestdict=False" to fix compatibility with Cython 0.13.

--- djvu/decode.pyx
+++ djvu/decode.pyx
@@ -9,6 +9,8 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 # General Public License for more details.
 
+#cython: autotestdict=False
+
 '''
 DjVuLibre bindings: module for efficiently decoding and displaying DjVu documents.
 
