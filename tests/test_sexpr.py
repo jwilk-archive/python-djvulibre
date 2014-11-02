3e8074fbbc998f1bef686c428a7b0ef8ed6acab2
Jakub Wilk <jwilk@jwilk.net>
2014-11-02 17:41:24 +0100

    Update copyright information.

--- tests/test_sexpr.py
+++ tests/test_sexpr.py
@@ -1,6 +1,6 @@
 # encoding=UTF-8
 
-# Copyright © 2007-2012 Jakub Wilk <jwilk@jwilk.net>
+# Copyright © 2007-2014 Jakub Wilk <jwilk@jwilk.net>
 #
 # This package is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
