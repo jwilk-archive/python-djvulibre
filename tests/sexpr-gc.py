d6d630c83e660c9fd98ae3e751db48ed1ca13d34
Jakub Wilk <jwilk@jwilk.net>
2015-06-23 16:57:14 +0200

    tests/sexpr-gc: use lowercase variables.

--- tests/sexpr-gc.py
+++ tests/sexpr-gc.py
@@ -19,20 +19,20 @@ if __name__ != '__main__':
 import djvu.sexpr
 import os
 
-PROC_STATUS = '/proc/{pid}/status'.format(pid=os.getpid())
-SCALE = dict(kB=1024)
+proc_status = '/proc/{pid}/status'.format(pid=os.getpid())
+scale = dict(kB=1024)
 
 def mem_info(key='VmSize'):
     try:
-        file = open(PROC_STATUS)
+        file = open(proc_status)
         for line in file:
             if line.startswith('{key}:'.format(key=key)):
                 _, value, unit = line.split(None, 3)
-                return int(value) * SCALE[unit]
+                return int(value) * scale[unit]
     finally:
         file.close()
 
-STEP = 1 << 17
+step = 1 << 17
 
 try:
     range = xrange
@@ -43,6 +43,6 @@ n = 0
 while True:
     mb = mem_info() / (1 << 20)
     print('{mb:.2f}M'.format(mb=mb))
-    [djvu.sexpr.Expression(4) for i in range(STEP)]
+    [djvu.sexpr.Expression(4) for i in range(step)]
 
 # vim:ts=4 sw=4 et
