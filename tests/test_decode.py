4a3f38a1ab7a77125b25de4c51618240bdbc4dfc
Jakub Wilk <jwilk@jwilk.net>
2015-08-12 01:43:41 +0200

    tests/test_decode: mark a string as raw.

--- tests/test_decode.py
+++ tests/test_decode.py
@@ -526,7 +526,7 @@ class test_page_jobs():
         with assert_raises_str(ValueError, 'row_alignment must be a positive integer'):
             page_job.render(RENDER_COLOR, (0, 0, 10, 10), (0, 0, 10, 10), PixelFormatRgb(), -1)
 
-        with assert_raises_regexp(MemoryError, '\AUnable to allocate [0-9]+ bytes for an image memory\Z'):
+        with assert_raises_regexp(MemoryError, r'\AUnable to allocate [0-9]+ bytes for an image memory\Z'):
             x = int((maxsize//2) ** 0.5)
             page_job.render(RENDER_COLOR, (0, 0, x, x), (0, 0, x, x), PixelFormatRgb(), 8)
 
