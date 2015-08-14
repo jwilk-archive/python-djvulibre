c91ab2e71d8beef4db385299774868174c68cfbf
Jakub Wilk <jwilk@jwilk.net>
2015-08-14 12:36:55 +0200

    Update license statements.

--- doc/api/conf.py
+++ doc/api/conf.py
@@ -1,5 +1,18 @@
 # encoding=UTF-8
 
+# Copyright © 2009-2015 Jakub Wilk <jwilk@jwilk.net>
+#
+# This file is part of python-djvulibre.
+#
+# python-djvulibre is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 2
+# as published by the Free Software Foundation.
+#
+# python-djvulibre is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+
 import os
 import codecs
 
