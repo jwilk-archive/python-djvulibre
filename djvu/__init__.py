d14b7d7a6cbbb60da27393db730408c77a22041b
Jakub Wilk <jwilk@jwilk.net>
2021-02-26 17:33:53 +0100

    Simplify Python version check.

--- djvu/__init__.py
+++ djvu/__init__.py
@@ -13,9 +13,6 @@
 # or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
 # more details.
 
-import sys
-
-if sys.version_info < (2, 6):
-    raise RuntimeError('Python >= 2.6 is required')
+type(b'')  # Python >= 2.6 is required
 
 # vim:ts=4 sts=4 sts=4 sw=4 et
